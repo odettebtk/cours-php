1e129779f54d34f4411832544eba5b5e6f909e62
odette <>
2014-10-14 10:37:57 +0200

    rajout des test sur index.php

--- index.php
+++ index.php
@@ -1,17 +1,58 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <title>essai de page</title>
+        <meta charset="windows-1252">
+        <meta name="viewport" content="width=device-width">
     </head>
     <body>
         <?php
-        // put your code here
+        $sNOM="braive";
+        $sPRENOM="Xavier";
+        $sRUE=" de la Sirène";
+        $iNo=2;
+        $iCP=4000;
+        $sVille="liège";
+        $ftotalHT=457.00;
+        $result=1+1;
+        echo $result/2;
+        print_r($_POST);
+        //filter_input(input_get,"nom")
+        //$_GET["nom"]
         ?>
+        
+        <form action="index.php" method="get">
+          nom:<input type="text" name="nom" value="BOT" />
+          prenom:<input type="text" name="prenom" value="Odette" />
+          <input type="submit" value="buton" name="envoyer" />
+        </form>
+        <table border="1">
+          
+            <tbody>
+                <tr>
+                    <td> Client </td>
+                    <td><?php echo $sPRENOM." ".$sNOM ?> </td>
+                </tr>
+                <tr>
+                    <td>Adresse </td>
+                    <td><?php echo $iNo.",".$iCP ?> <br> <?php echo $sRUE." ".$sVille ?> </td>
+                </tr>
+                
+                             
+                <tr>
+                    <td>Montant de la commande HT </td>
+                   <td><?php echo $ftotalHT ?> </td>
+                </tr>
+            </tbody>
+        </table>
+        
+        
+   <form action=" .php" method="get">
+       <label> Login </label>
+       <input> </input> <br>
+       
+       <label> passe </label>
+       <input> </input>
+   </form>
     </body>
 </html>
